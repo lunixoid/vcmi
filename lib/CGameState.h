1eb7ec79938e4e00b880ea52dee0f527749c5ee9
AlexVinS <alexvins@mail.ru>
2017-08-07 13:31:35 +0300

    Draft of GameState integration/regression tests * [tests] WIP Added mock for MapService * Ignore mod settings and load only "vcmi" mod when running tests * fixed https://bugs.vcmi.eu/view.php?id=2765 (with tests) * Cleaned up battleAI * Cleaned up spell effects API * Added Clone effect * Draft teleport effect * extracted skeleton for independent CStack state * stack state changes used in AI stack value evaluation * More battle AI magic improvements * tweaks

--- lib/CGameState.h
+++ lib/CGameState.h
@@ -54,6 +54,7 @@ class CQuest;
 class CCampaignScenario;
 struct EventCondition;
 class CScenarioTravel;
+class IMapService;
 
 namespace boost
 {
@@ -152,7 +153,7 @@ public:
 	CGameState();
 	virtual ~CGameState();
 
-	void init(StartInfo * si, bool allowSavingRandomMap = false);
+	void init(const IMapService * mapService, StartInfo * si, bool allowSavingRandomMap = false);
 
 	ConstTransitivePtr<StartInfo> scenarioOps, initialOpts; //second one is a copy of settings received from pregame (not randomized)
 	PlayerColor currentPlayer; //ID of player currently having turn
@@ -245,8 +246,8 @@ private:
 
 	// ----- initialization -----
 
-	void initNewGame(bool allowSavingRandomMap);
-	void initCampaign();
+	void initNewGame(const IMapService * mapService, bool allowSavingRandomMap);
+	void initCampaign(const IMapService * mapService);
 	void checkMapChecksum();
 	void initGrailPosition();
 	void initRandomFactionsForPlayers();
--- lib/battle/BattleInfo.cpp
+++ lib/battle/BattleInfo.cpp
@@ -51,7 +51,7 @@ std::pair< std::vector<BattleHex>, int > BattleInfo::getPath(BattleHex start, Ba
 ui32 BattleInfo::calculateDmg(const CStack * attacker, const CStack * defender,
 	bool shooting, ui8 charge, bool lucky, bool unlucky, bool deathBlow, bool ballistaDoubleDmg, CRandomGenerator & rand)
 {
-	BattleAttackInfo bai(attacker, defender, shooting);
+	BattleAttackInfo bai(attacker, defender, attacker, defender, shooting);
 	bai.chargedFields = charge;
 	bai.luckyHit = lucky;
 	bai.unluckyHit = unlucky;
@@ -93,7 +93,6 @@ CStack * BattleInfo::generateNewStack(const CStackInstance & base, ui8 side, Slo
 
 	auto ret = new CStack(&base, owner, stackID, side, slot);
 	ret->position = getAvaliableHex(base.getCreatureID(), side, position); //TODO: what if no free tile on battlefield was found?
-	ret->state.insert(EBattleStackState::ALIVE); //alive state indication
 	return ret;
 }
 
@@ -103,7 +102,6 @@ CStack * BattleInfo::generateNewStack(const CStackBasicDescriptor & base, ui8 si
 	PlayerColor owner = sides[side].color;
 	auto ret = new CStack(&base, owner, stackID, side, slot);
 	ret->position = position;
-	ret->state.insert(EBattleStackState::ALIVE); //alive state indication
 	return ret;
 }
 
